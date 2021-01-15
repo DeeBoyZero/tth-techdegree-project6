bac7e6f4a99c50a610428cf04003d321b7b74341
Mathieu Desilets <mathieu.desilets@gmail.com>
2021-01-15 15:47:44 -0500

    add projects images + error handling

--- app.js
+++ app.js
@@ -1,32 +1,45 @@
+// Set the express app variable
 const express = require('express');
 const app = express();
+// Path module used in settings the views and templating engine
 const path = require('path');
+// Import the router module
 const router = require('./routes/index');
+// Set the port for the app
 const port = 3000;
 
+// Setting the path for the views and templating engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Setting a static route for the static server
 app.use('/static', express.static('public'));
+
+// Setup the app to use the router module
 app.use('/', router);
 
+// 404 error handler
 app.use((req, res, next) => {
-    const err = new Error('Page Not Found');
+    const err = new Error('Looks like the page you requested does not exist');
     err.status = 404;
+    console.log(`${err.message} - Error: ${err.status}`);
     next(err);
 });
 
+// General errors hanler
 app.use((err, req, res, next) => {
     if(err.status === 404) {
       res.status = 404;
-      res.send(`<h1>${err.message} ${err.status}</h1><p>${err.stack}</p>`);
+      res.render('page-not-found', {err});
     } else {
-      err.message = err.message || `Oops! It looks like something went wrong on the server.`;
-      res.status(err.status || 500) 
-      res.send(`<h1>${err.message} ${err.status}</h1><p>${err.stack}</p>`);
+      err.message = err.message || `Oops! Something went wrong on the server.`;
+      res.status(err.status || 500)
+      console.log(`${err.message} - Error: ${err.status}`); 
+      res.render('error', {err});
     }
 });
 
+// Setup the listening port for the app
 app.listen(`${port}`, () => {
     console.log(`The app is listening on port: ${port}`);
-})
+});
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,49 @@
+// Setup the express app variable
 const express = require('express');
+// Intialize the express router
 const router = express.Router();
+// Import the projects data
 const { projects } = require('../data.json');
 
-
+// Root route handler
 router.get('/', (req, res) => {
     res.render('index', {projects});
 });
 
+// About route handler
 router.get('/about', (req, res) => {
     res.render('about');
 });
 
-router.get('/project/:id', (req, res) => {
-    res.render('project');
+// Projects dynamics route handler
+router.get('/project/:id', (req, res, next) => {
+    if (projects[req.params.id]) {
+        const projectId = req.params.id;
+        const project = projects.find(({id}) => id === +projectId);
+        res.render('project', {project});
+    } else {
+        const err = new Error();
+        err.status = 404;
+        err.message = 'Looks like the page you requested does not exist';
+        console.log(`${err.message} - Error: ${err.status}`);
+        next(err);
+    }
 });
 
+// Projects dynamics route handler ( accept both project and projects )
 router.get('/projects/:id', (req, res) => {
-    res.render('project');
+    if (projects[req.params.id]) {
+        const projectId = req.params.id;
+        const project = projects.find(({id}) => id === +projectId);
+        res.render('project', {project});
+    } else {
+        const err = new Error();
+        err.status = 404;
+        err.message = 'Looks like the page you requested does not exist';
+        console.log(`${err.message} - Error: ${err.status}`);
+        next(err);
+    }
 });
 
-
-
+// export the router module
 module.exports = router;
